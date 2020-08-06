18754e014687f69ea6853cdd22b75fec6fd882f5
LeeJinbeom <jinbomang@gmail.com>
2020-08-07 01:39:55 +0900

    연습코드수정

--- first/models.py
+++ first/models.py
@@ -3,6 +3,5 @@ from django.db import models
 # Create your models here.
 class Students(models.Model):
     name = models.CharField(max_length=10)
-    math = models.IntegerField()
-    science = models.IntegerField()
-    english = models.IntegerField()
+    address = models.CharField(max_length=50)
+    email = models.CharField(max_length=30)
